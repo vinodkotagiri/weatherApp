bde4e35ddcf601e8b8775f843c16dc6bed5b8175
vinodkotagiri <vinodkotagiri@icloud.com>
2022-06-17 22:20:09 +0530

    add event listeners to search btn and return btn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-
 function getData(location) {
     fetch(`https://api.weatherapi.com/v1/current.json?key=d56b027f0d0c4b16b2b144656221706&q=${location}&aqi=yes`)
         .then(response => response.json())
         .then(data => initialize(data))
         .catch(error => console.log(error))
         .finally(console.log("fetched successfully!"));
+
     function initialize(data) {
         let locationData = data;
         updateCurrentWeather(locationData);
@@ -12,11 +12,9 @@ function getData(location) {
     }
 }
 
-getData("Hyderabad");
-
-function updateCurrentWeather(data){
-    let currentWeather=document.getElementById("currentWeatherData");
-let html=`<div class="row">
+function updateCurrentWeather(data) {
+    let currentWeather = document.getElementById("currentWeatherData");
+    let html = `<div class="row">
 <div class="col city-wrapper">
     <h1 class="city" id="city">${data.location.name}</h1>
 </div>
@@ -34,10 +32,29 @@ let html=`<div class="row">
 </div>
 <div class="col other-details-wrapper">
     <h5 class="wind">Wind</h5>
-    <p>Speed-  ${data.current.wind_kmph} kmph</p>
-    <p>Direction - ${data.current.wind_deg} ${data.current.wind_dir}</p>
+    <p>Speed-  ${data.current.wind_kph} kmph</p>
+    <p>Direction - ${data.current.wind_degree} ${data.current.wind_dir}</p>
     <h5 class="aqi">Air Quality Index - ${data.current.air_quality["us-epa-index"]}</h5>
 </div>
 </div>`
-currentWeather.innerHTML=html;
+    currentWeather.innerHTML = html;
 }
+
+function UpdateUI() {
+    let search = document.getElementById("searchField");
+    let searchBtn = document.getElementById("searchBtn");
+
+    searchBtn.addEventListener("click", () => {
+        let locationToSearch = search.value;
+        getData(locationToSearch);
+        console.log("clicked");
+    })
+    search.addEventListener("keypress", (event) => {
+        if (event.key == "Enter") {
+            let locationToSearch = search.value;
+            getData(locationToSearch);
+            console.log("clicked");
+        }
+    })
+}
+UpdateUI();
